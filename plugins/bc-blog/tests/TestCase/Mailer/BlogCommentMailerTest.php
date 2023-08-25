99943d31b0772095133349b940b2f51312e432cf
nghiem-mb <nghiem@mediabridge.asia>
2023-08-25 15:18:39 +0700

    initial

---
+++ plugins/bc-blog/tests/TestCase/Mailer/BlogCommentMailerTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * baserCMS :  Based Website Development Project <https://basercms.net>
+ * Copyright (c) NPO baser foundation <https://baserfoundation.org/>
+ *
+ * @copyright     Copyright (c) NPO baser foundation
+ * @link          https://basercms.net baserCMS Project
+ * @since         5.0.0
+ * @license       https://basercms.net/license/index.html MIT License
+ */
+
+namespace BcBlog\Test\TestCase\Mailer;
+
+use BaserCore\TestSuite\BcTestCase;
+use BcBlog\Mailer\BlogCommentMailer;
+
+/**
+ * BlogCommentMailerTest
+ * @property  BlogCommentMailer $BlogCommentMailer
+ *
+ */
+class BlogCommentMailerTest extends BcTestCase
+{
+    /**
+     * set up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * tearDown
+     *
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+
+    /**
+     * test sendCommentToAdmin
+     */
+    public function test_sendCommentToAdmin()
+    {
+        //準備
+
+        //正常系実行
+
+        //異常系実行
+
+
+    }
+
+    /**
+     * test sendCommentToUser
+     */
+    public function test_sendCommentToUser()
+    {
+        //準備
+
+        //正常系実行
+
+        //異常系実行
+
+
+    }
+
+}
