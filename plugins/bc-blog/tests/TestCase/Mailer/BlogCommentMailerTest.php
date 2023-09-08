d01ffbe7f01a23fddf55f4594f35d3834f5589bb
nghiem-mb <nghiem@mediabridge.asia>
2023-09-08 09:52:57 +0700

    set up

--- plugins/bc-blog/tests/TestCase/Mailer/BlogCommentMailerTest.php
+++ plugins/bc-blog/tests/TestCase/Mailer/BlogCommentMailerTest.php
@@ -11,8 +11,21 @@
 
 namespace BcBlog\Test\TestCase\Mailer;
 
+use BaserCore\Service\ContentsService;
+use BaserCore\Service\ContentsServiceInterface;
+use BaserCore\Test\Scenario\InitAppScenario;
 use BaserCore\TestSuite\BcTestCase;
 use BcBlog\Mailer\BlogCommentMailer;
+use BcBlog\Model\Entity\BlogPost;
+use BcBlog\Service\BlogCommentsService;
+use BcBlog\Service\BlogCommentsServiceInterface;
+use BcBlog\Service\BlogPostsService;
+use BcBlog\Service\BlogPostsServiceInterface;
+use BcBlog\Test\Scenario\BlogCommentsServiceScenario;
+use BcBlog\Test\Scenario\BlogContentScenario;
+use Cake\Routing\Router;
+use Cake\TestSuite\IntegrationTestTrait;
+use CakephpFixtureFactories\Scenario\ScenarioAwareTrait;
 
 /**
  * BlogCommentMailerTest
@@ -21,6 +34,12 @@ use BcBlog\Mailer\BlogCommentMailer;
  */
 class BlogCommentMailerTest extends BcTestCase
 {
+
+    /**
+     * Trait
+     */
+    use ScenarioAwareTrait;
+    use IntegrationTestTrait;
     /**
      * set up
      *
@@ -47,6 +66,66 @@ class BlogCommentMailerTest extends BcTestCase
      */
     public function test_sendCommentToAdmin()
     {
+        $this->loadFixtureScenario(InitAppScenario::class);
+        $this->loadFixtureScenario(
+            BlogContentScenario::class,
+            1,  // id
+            1, // siteId
+            null, // parentId
+            'news1', // name
+            '/news/' // url
+        );
+        $this->loadFixtureScenario(BlogCommentsServiceScenario::class);
+        $blogCommentService = $this->getService(BlogCommentsServiceInterface::class);
+        $blogComment = $blogCommentService->get(1);
+        $blogPostsService = $this->getService(BlogPostsServiceInterface::class);
+        $blogPost = $blogPostsService->get(1);
+        $contentService = $this->getService(ContentsServiceInterface::class);
+        $content = $blogPost->blog_content->content;
+        $site = $content->site;
+        $postUrl = $contentService->getUrl($content->url .  '/archives/' . $blogPost->no, true, $site->use_subdomain);
+        try {
+            $result = $this->execPrivateMethod($blogCommentService, 'getMailer("BcBlog.BlogComment")->send',
+                [
+                    'sendCommentToAdmin',
+                    [
+                        $site->title,
+                        [
+                            'blogComment' => $blogComment,
+                            'blogPost' => $blogPost,
+                            'contentTitle' => $content->title,
+                            'postUrl' => $postUrl,
+                            'adminUrl' => Router::url([
+                                'plugin' => 'BcBlog',
+                                'prefix' => 'Admin',
+                                'controller' => 'BlogComments',
+                                'action' => 'index',
+                                $blogComment->blog_content_id
+                            ], true)
+                        ]
+                    ]
+                ]);
+//            $result = $blogCommentService->getMailer('BcBlog.BlogComment')->send('sendCommentToAdmin',
+//                [
+//                    $site->title,
+//                    [
+//                        'blogComment' => $blogComment,
+//                        'blogPost' => $blogPost,
+//                        'contentTitle' => $content->title,
+//                        'postUrl' => $postUrl,
+//                        'adminUrl' => Router::url([
+//                            'plugin' => 'BcBlog',
+//                            'prefix' => 'Admin',
+//                            'controller' => 'BlogComments',
+//                            'action' => 'index',
+//                            $blogComment->blog_content_id
+//                        ], true)
+//                    ]
+//                ]);
+            dd($result);
+        } catch (\Throwable $e) {
+            throw $e;
+        }
         //準備
 
         //正常系実行
